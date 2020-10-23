fdaf9eb1d7326e16bce7a219029e469badd7d5ae
Samil Ayoub <ssamil.ayoub@gmail.com>
2020-10-23 15:11:45 +0100

    Feat/ add spring boot runner

--- src/main/java/com/example/consumingquotes/DemoApplication.java
+++ src/main/java/com/example/consumingquotes/DemoApplication.java
@@ -1,13 +1,37 @@
 package com.example.consumingquotes;
 
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class DemoApplication {
 
+	private static final Logger log = LoggerFactory.getLogger(DemoApplication.class);
+
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
+	
+	@Bean
+	public RestTemplate restTemplate(RestTemplateBuilder builder) {
+		return builder.build();
+	}
+	
+	@Bean
+	public CommandLineRunner run(RestTemplate restTemplate) throws Exception {
+		return args -> {
+			String url = "https://gturnquist-quoters.cfapps.io/api/random";
+			Quote quote = restTemplate.getForObject(url, Quote.class);
+			log.info(quote.toString());
+		};
+	}
+	
 
 }
